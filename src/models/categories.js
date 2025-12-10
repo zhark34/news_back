8357d74d93967ec9bd2640df42072b07794a1f0b
evilrushgame <traviesogonzalo@gmail.com>
2025-12-10 12:41:34 -0300

    correcion en categorias

---
+++ src/models/categories.js
@@ -0,0 +1,28 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/db.js";
+
+const Categories = sequelize.define(
+    "categories",
+    {
+
+        id: {
+            type: DataTypes.BIGINT,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        category: {
+            type: DataTypes.STRING(50),
+            allowNull: false,
+        },
+        journalist_id: {
+            type: DataTypes.BIGINT,
+            allowNull: false,
+        },
+
+    },
+    {
+        timestamps: true
+    }
+)
+
+export default Categories;
