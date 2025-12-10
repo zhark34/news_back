475598b6de8ce60183515b382f655d40cb585b84
evilrushgame <traviesogonzalo@gmail.com>
2025-12-10 10:11:42 -0300

    db colocado en config

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import rateLimit from "express-rate-limit";
-import sequelize from "./db.js";
+import sequelize from "./src/config/db.js";
 
 dotenv.config();
 
