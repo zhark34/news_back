ec3feadeacaf434589498c3adbe0645e18de6d38
evilrushgame <traviesogonzalo@gmail.com>
2025-12-10 10:15:44 -0300

    agregado modelo news_block

---
+++ src/models/news.block.js
@@ -0,0 +1,35 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/db.js";
+
+const NewsBlock = sequelize.define(
+    "news_block",
+    {
+
+        id: {
+            type: DataTypes.BIGINT,
+            primaryKey: true,
+        },
+        news_id: {
+            type: DataTypes.BIGINT,
+            primaryKey: true,
+        },
+        block_type: {
+            type: DataTypes.STRING(50),
+            allowNull: false,
+        },
+        block_id: {
+            type: DataTypes.BIGINT,
+            primaryKey: true,
+        },
+        position: {
+            type: DataTypes.SMALLINT,
+            primaryKey: true,
+        },
+
+    },
+    {
+        timestamps: true
+    }
+)
+
+export default NewsBlock;
