ba26569feefe9ca86cf9f696b60219f63e95bb30
evilrushgame <traviesogonzalo@gmail.com>
2025-12-11 13:39:31 -0300

    cambio en como se verifica si hay periodistas

--- src/services/journalist.get.one.services.js
+++ src/services/journalist.get.one.services.js
@@ -5,7 +5,7 @@ export const getOneJournalistServices = async (id) =>{
 
     const allJournalist = await Journalist.findOne({where: { journalist_id: id },include: { all: true }, attributes: { exclude: ['password', 'reset_token', 'reset_token_expire'] }});
 
-    if(allJournalist.length === 0){
+    if(!allJournalist){
 
         throw new Error("NO_JOURNALIST_FOUND");
 
