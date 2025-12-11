b1f3d7132049e99d19310368040cc77a97db1cb2
evilrushgame <traviesogonzalo@gmail.com>
2025-12-11 15:41:23 -0300

    agregado servicio para obtener periodista con querys

---
+++ src/services/journalist.get.filter.services.js
@@ -0,0 +1,45 @@
+import Journalist from "../models/journalist.js";
+import CategoriesJournalist from '../models/categories.journalist.js'
+import Categories from '../models/categories.js'
+import News from '../models/news.js'
+import SocialNetworks from '../models/social.networks.js'
+
+export const getOneJournalistFilterServices = async (filters) => {
+
+
+    const journalistWhere = {};
+
+    if (filters.journalist_id) journalistWhere.journalist_id = filters.journalist_id;
+    if (filters.name) journalistWhere.name = filters.name;
+    if (filters.email) journalistWhere.email = filters.email;
+    if (filters.role) journalistWhere.role = filters.role;
+
+    const categoiesWhere = {};
+
+    if (filters.categories) categoiesWhere.category = filters.categories;
+
+    const journalist = await Journalist.findAll(
+        {where: journalistWhere,
+        attributes: { exclude: ['password', 'reset_token', 'reset_token_expire']},
+
+        include: [
+            {
+            model: CategoriesJournalist,
+            include: [
+                {
+                    model: Categories,
+                    attributes: ['category', 'category_id']
+                }
+            ]
+        },
+        { model: SocialNetworks }, 
+        { model: News }
+        ]}
+    )
+
+    if (!journalist) {
+        throw new Error("NO_JOURNALIST_FOUND");
+    }
+
+    return journalist;
+};
