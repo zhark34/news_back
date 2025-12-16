eaa1097fef2bd0ffa9dffbbe3ef0c0bb405df7e4
evilrushgame <traviesogonzalo@gmail.com>
2025-12-16 15:45:08 -0300

    Mejorada la robustez de la validación de periodista

--- src/services/journalist.validate.services.js
+++ src/services/journalist.validate.services.js
@@ -1,14 +1,56 @@
 import Journalist from "../models/journalist.js";
+import Session from "../models/sessions.js";
+import { hashRefreshToken } from "../utils/hash.refresh.token.js";
 
-export const validateJournalistServices = async (id, email, role) => {
+export const validateJournalistServices = async (id, refreshToken) => {
 
     const journalist = await Journalist.findOne({
-        where: { journalist_id: id, email, role }
+        where: { journalist_id: id }
     });
 
     if (!journalist) {
         throw new Error("JOURNALIST_NOT_FOUND");
     }
 
-    return journalist;
+    const refreshTokenHash = hashRefreshToken(refreshToken);
+
+    const session = await Session.findOne({
+
+        where: {
+
+            refresh_token_hash: refreshTokenHash
+
+        }
+
+    })
+
+    if (!session) {
+        throw new Error("INVALID_REFRESH_TOKEN");
+    }
+
+    if (new Date(session.expires_at) < new Date()) {
+        await Session.destroy({where: {refresh_token_hash: refreshTokenHash}})
+        throw new Error("REFRESH_TOKEN_EXPIRED");
+    }
+
+    if (session.revoke === true) {
+        await Session.destroy({where: {refresh_token_hash: refreshTokenHash}})
+        throw new Error("DISABLED_TOKEN");
+    }
+
+    if (journalist.journalist_id !== session.journalist_id){
+
+        throw new Error("ID_DOES_NOT_MATCH");
+
+    }
+
+    return {
+        
+        id: journalist.journalist_id,
+        name: journalist.name,
+        bio: journalist.bio,
+        photo: journalist.profile_image_url,
+        role: journalist.role
+
+    };
 };
