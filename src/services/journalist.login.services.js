e7a63accddf941ba5d054b9680f4d8bb63aaf9ab
evilrushgame <traviesogonzalo@gmail.com>
2025-12-16 15:43:17 -0300

    feat(auth): agregar creación de sesiones y mejorar respuesta de login
    - llamado al modelo Sessions
    - llamado a generateId, generateRefreshToken, sendEmail y loginAlertEmail
    - agregado creación de sesiones
    - cambio en la generación del payload, ahora incluye role e id_token
    - agregado refresh token y foto de perfil en la respuesta

--- src/services/journalist.login.services.js
+++ src/services/journalist.login.services.js
@@ -1,9 +1,13 @@
 import Journalist from "../models/journalist.js"
+import Session from "../models/sessions.js"
 import { validatePassword } from '../utils/validate.password.js'
 import { generateToken } from "../utils/generate.token.js"
+import { generateId } from "../utils/id.generator.js"
+import { generateRefreshToken } from '../utils/generate.refresh.token.js'
+import { sendEmail } from "../config/nodemailer.js"
+import { loginAlertEmail } from "../utils/email_templates/email.alert.session.js"
 
-export const loginJournalistServices = async (email, password) =>{
-
+export const loginJournalistServices = async (email, password, device, ip, country, province, city) =>{
 
     const validateJournalist = await Journalist.findOne({where: { email }})
 
@@ -21,11 +25,38 @@ export const loginJournalistServices = async (email, password) =>{
 
     }
 
+    const id_token = generateId();
+
+    const {refreshToken, hashed} = await generateRefreshToken();
+
+    const newSession = await Session.create({
+
+        journalist_id: validateJournalist.journalist_id,
+        id_token: id_token,
+        refresh_token_hash: hashed,
+        user_agent: device.browser,
+        ip: ip,
+        location_country: country,
+        location_region: province,
+        location_city: city,
+        expires_at: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)
+
+    })
+
     const payload = {
         journalist_id: validateJournalist.journalist_id,
         email: validateJournalist.email,
-        role: validateJournalist.role
+        role: validateJournalist.role,
+        id_token: id_token
     };
+
+    await sendEmail({
+
+        to: validateJournalist.email, 
+        subject: "Alerta de inicio de sesión",
+        html: loginAlertEmail(validateJournalist.name, "linkreferencia.com", ip, country, province, city)
+
+    })
 
     const token = await generateToken(payload);
 
@@ -33,9 +64,11 @@ export const loginJournalistServices = async (email, password) =>{
 
         message: "Login exitoso",
         token: token,
+        refresh_token: refreshToken,
         id: validateJournalist.journalist_id,
         name: validateJournalist.name,
-        role: validateJournalist.role
+        role: validateJournalist.role,
+        photo: validateJournalist.profile_image_url
 
     }
 
