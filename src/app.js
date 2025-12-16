ef939b1741e50433063b9fa2120f858fb0e2e913
evilrushgame <traviesogonzalo@gmail.com>
2025-12-16 15:33:18 -0300

    llamado cors, cookie parser. definida ruta de redes sociales

--- src/app.js
+++ src/app.js
@@ -1,16 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
 import rateLimit from "express-rate-limit";
+import cors from "cors";
+import cookieParser from 'cookie-parser';
 import journalistRoutes from './routes/journalist.routes.js'
+import socialNetworks from "./routes/social.networks.routes.js";
 
 dotenv.config();
 
 const app = express();
+app.use(cors({
+    origin: 'http://localhost:3001',
+    credentials: true}));
 app.use(express.json());
+app.use(cookieParser());
 
 const limiter = rateLimit({
 	windowMs: 15 * 60 * 1000,
-	max: 100,
+	max: 1000,
     message: {
         status: 429,
         error: 'Demasiadas peticiones. Por favor, intenta de nuevo en 15 minutos.'
@@ -22,6 +29,8 @@ const limiter = rateLimit({
 app.use(limiter);
 
 app.use('/api/v1/journalist', journalistRoutes)
+
+app.use('/api/v1/social-networks', socialNetworks)
 
 app.get('/', (req, res) => {
   res.send('Servidor funcionando');
