aeb236c56fb4537dde61f53e64a1a2526675d545
evilrushgame <traviesogonzalo@gmail.com>
2025-12-21 21:53:03 -0300

    agregado controller para agregar categoria

--- src/controllers/categories.controllers.js
+++ src/controllers/categories.controllers.js
@@ -1,5 +1,5 @@
 import { getAllCategoriesServices } from "../services/categories.get.all.services.js"
-
+import { createCategoriesServices } from "../services/categories.create.services.js"
 
 export const getAllCategories = async (req, res, next) =>{
 
@@ -20,5 +20,28 @@ export const getAllCategories = async (req, res, next) =>{
         }
 
         return res.status(500).json({ message: "Error al obtener las categorias" });
+    }
+};
+
+export const createCategories = async (req, res, next) =>{
+
+    const {categorie} = req.body;
+
+    try{
+
+        const categories = await createCategoriesServices(categorie);
+
+        return res.status(200).json({
+            message: categories
+        });
+
+    } catch (error) {
+        console.error(error);
+
+        if (error.message === "CATEGORIES_ALREDY_EXIST") {
+            return res.status(404).json({ message: "Categoria ya existe" });
+        }
+
+        return res.status(500).json({ message: "Error al crear la categoria" });
     }
 };
