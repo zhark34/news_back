2c74c1c9f658a32545152cdd0323fbd53ac3251b
evilrushgame <traviesogonzalo@gmail.com>
2025-12-21 21:53:17 -0300

    agregado ruta para agregar categoria

--- src/routes/categories.routes.js
+++ src/routes/categories.routes.js
@@ -3,12 +3,15 @@ const router = Router();
 
 import { 
     
-    getAllCategories
+    getAllCategories,
+    createCategories
 
 } from "../controllers/categories.controllers.js";
 
 import { tokenMiddleware } from "../middlewares/token.middlewares.js";
 
 router.get("/all", getAllCategories)
+
+router.post("/create", tokenMiddleware, createCategories)
 
 export default router;
