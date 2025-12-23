375af9f2be1618158716659b45f6635e981e4b4a
evilrushgame <traviesogonzalo@gmail.com>
2025-12-23 15:37:45 -0300

    controller para agregar categoria a periodista

--- src/controllers/categories.controllers.js
+++ src/controllers/categories.controllers.js
@@ -1,5 +1,6 @@
 import { getAllCategoriesServices } from "../services/categories.get.all.services.js"
 import { createCategoriesServices } from "../services/categories.create.services.js"
+import { addCategoriesJournalistServices } from "../services/categories.add.journalist.services.js"
 
 export const getAllCategories = async (req, res, next) =>{
 
@@ -30,6 +31,31 @@ export const createCategories = async (req, res, next) =>{
     try{
 
         const categories = await createCategoriesServices(categorie);
+
+        return res.status(200).json({
+            message: categories
+        });
+
+    } catch (error) {
+        console.error(error);
+
+        if (error.message === "CATEGORIES_ALREDY_EXIST") {
+            return res.status(404).json({ message: "Categoria ya existe" });
+        }
+
+        return res.status(500).json({ message: "Error al crear la categoria" });
+    }
+};
+
+export const addCategoriesJournalist = async (req, res, next) =>{
+
+    const {id, journalist_id} = req.body;
+
+    
+
+    try{
+
+        const categories = await addCategoriesJournalistServices(id, journalist_id);
 
         return res.status(200).json({
             message: categories
