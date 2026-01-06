899b39577bec258309d9bc6c11a7c3dd27f4b15a
evilrushgame <traviesogonzalo@gmail.com>
2026-01-06 20:08:12 -0300

    agregado servicio de creación de categorías con autorización de administrador.

--- src/services/categories.create.services.js
+++ src/services/categories.create.services.js
@@ -1,16 +1,31 @@
 import Categories from '../models/categories.js'
+import Journalist from '../models/journalist.js'
 
-export const createCategoriesServices = async (categorie) =>{
+export const createCategoriesServices = async (categorie, admin_id) => {
 
-    const checkCategories = await Categories.findOne({where: {category: categorie}});
+    const checkCategories = await Categories.findOne({ where: { category: categorie } });
 
-    if(checkCategories){
+    if (checkCategories) {
 
         throw new Error("CATEGORIES_ALREDY_EXIST");
 
     }
 
-    await Categories.create({category_id: Date.now() ,category: categorie})
+    const checkAdmin = await Journalist.findOne({ where: { journalist_id: admin_id } });
+
+    if (!checkAdmin) {
+
+        throw new Error("ADMIN_NO_EXIST");
+
+    }
+
+    if (checkAdmin.role !== "admin") {
+
+        throw new Error("ADMIN_NO_AUTH");
+
+    }
+
+    await Categories.create({ category_id: Date.now(), category: categorie })
 
     return "Categoria creada"
 
