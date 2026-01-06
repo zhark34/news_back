2cdfb86e03e2252a6c368c8b91367ef69ecf7d5f
evilrushgame <traviesogonzalo@gmail.com>
2026-01-06 17:30:14 -0300

    agregada validacion de rol del que solicita el cambio de rol

--- src/services/journalist.update.role.services.js
+++ src/services/journalist.update.role.services.js
@@ -1,12 +1,26 @@
 import Journalist from "../models/journalist.js";
 
-export const updateRoleJournalistServices = async (journalist_id, role) => {
+export const updateRoleJournalistServices = async (journalist_id, role, admin_id) => {
 
-    const checkJournalist = await Journalist.findOne({where: { journalist_id }})
+    const checkJournalist = await Journalist.findOne({ where: { journalist_id } })
 
-    if(!checkJournalist){
+    if (!checkJournalist) {
 
         throw new Error("JOURNALITS_NO_EXIST");
+
+    }
+
+    const checkAdmin = await Journalist.findOne({ where: { journalist_id: admin_id } })
+
+    if (!checkAdmin) {
+
+        throw new Error("ADMIN_NO_EXIST");
+
+    }
+
+    if (checkAdmin.role !== "admin") {
+
+        throw new Error("ADMIN_NO_AUTH");
 
     }
 
