a2438781f6243c19b66f9213d194b28726b146ae
evilrushgame <traviesogonzalo@gmail.com>
2026-01-06 20:07:25 -0300

    agregado rutas de eliminar y editar categorias

--- src/routes/categories.routes.js
+++ src/routes/categories.routes.js
@@ -7,7 +7,10 @@ import {
     createCategories,
     addCategoriesJournalist,
     getCategorieJournalist,
-    deleteCategoriesJournalist
+    deleteCategoriesJournalist,
+    editCategories,
+    deleteCategories
+
 } from "../controllers/categories.controllers.js";
 
 import { tokenMiddleware } from "../middlewares/token.middlewares.js";
@@ -17,6 +20,10 @@ import { refreshToken } from "../middlewares/refresh.token.js";
 router.get("/all", getAllCategories)
 
 router.post("/create", tokenMiddleware, refreshToken, createCategories)
+
+router.patch("/edit", tokenMiddleware, refreshToken, editCategories)
+
+router.delete("/delete/:id", tokenMiddleware, refreshToken, deleteCategories)
 
 router.post("/add", tokenMiddleware, refreshToken, addCategoriesJournalist)
 
