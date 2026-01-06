00139379d67713531b951c18072d337edcfaba73
evilrushgame <traviesogonzalo@gmail.com>
2026-01-06 05:02:47 -0300

    cambiado

--- src/middlewares/refresh.token.js
+++ src/middlewares/refresh.token.js
@@ -60,16 +60,16 @@ export const refreshToken = async (req, res, next) => {
 
         res.cookie("token", newToken, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: "strict",
+            secure: false,
+            sameSite: "lax",
             path: "/",
             maxAge: 15 * 60 * 1000
         });
 
         res.cookie("refresh_token", newRefreshToken, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: "strict",
+            secure: false,
+            sameSite: "lax",
             path: "/",
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
