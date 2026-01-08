c2d7bf84ce5ba5fdd893ecdc2076894ab639bc74
evilrushgame <traviesogonzalo@gmail.com>
2026-01-08 18:28:27 -0300

    agregado ruta para mandar noticia a aprobación

--- src/routes/news.routes.js
+++ src/routes/news.routes.js
@@ -4,7 +4,8 @@ import upload from "../middlewares/multer.config.js";
 
 import {
     createNews,
-    getNewsJournalist
+    getNewsJournalist,
+    newsSendToCheck
 } from "../controllers/news.controllers.js";
 
 import { tokenMiddleware } from "../middlewares/token.middlewares.js";
@@ -14,5 +15,7 @@ import { refreshToken } from "../middlewares/refresh.token.js";
 router.post("/create", tokenMiddleware, refreshToken, upload.single('photo'), createNews);
 
 router.get("/:status", tokenMiddleware, refreshToken, getNewsJournalist);
+
+router.patch("/send-to-check/:id", tokenMiddleware, refreshToken, newsSendToCheck);
 
 export default router;
