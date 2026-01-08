821597f0d8c120ddc448c1deca221e9f2a11bd62
evilrushgame <traviesogonzalo@gmail.com>
2026-01-08 20:06:25 -0300

    agregado controllers creacion de bloques de la noticia

--- src/controllers/news.controllers.js
+++ src/controllers/news.controllers.js
@@ -1,6 +1,8 @@
 import { createNewsService } from "../services/news.create.services.js";
 import { getNewsJournalistService } from "../services/news.get.journalist.services.js";
 import { newsSendToCheckService } from "../services/news.send.to.check.services.js";
+import { newsBlockCreateService } from "../services/news.block.create.services.js";
+import fs from 'fs-extra';
 
 export const createNews = async (req, res) => {
 
@@ -72,6 +74,34 @@ export const newsSendToCheck = async (req, res) => {
 
     try {
         const news = await newsSendToCheckService(journalistId, newsId);
+
+        return res.status(200).json({
+            message: "OK",
+            news
+        });
+
+    } catch (error) {
+
+        if (error.message === "JOURNALIST_NOT_FOUND") {
+            return res.status(404).json({ message: "No se encontró el periodista con la ID indicada" });
+        }
+
+        if (error.message === "NEWS_NOT_FOUND") {
+            return res.status(404).json({ message: "No se encontró la noticia con la ID indicada" });
+        }
+
+        return res.status(500).json({ message: "Error al obtener las noticias del periodista", error: error.message });
+    }
+};
+
+export const newsBlockCreate = async (req, res) => {
+
+    const journalistId = req.user.journalist_id;
+    const action = req.params.action;
+    const dataForm = req.body;
+
+    try {
+        const news = await newsBlockCreateService(journalistId, action, dataForm);
 
         return res.status(200).json({
             message: "OK",
