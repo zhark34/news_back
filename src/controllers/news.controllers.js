dd734293d3c0e694dd6451f5768c85b1d42dc615
evilrushgame <traviesogonzalo@gmail.com>
2026-01-08 18:27:08 -0300

    agregado controlador para mandar a aprovacion

--- src/controllers/news.controllers.js
+++ src/controllers/news.controllers.js
@@ -1,5 +1,6 @@
 import { createNewsService } from "../services/news.create.services.js";
 import { getNewsJournalistService } from "../services/news.get.journalist.services.js";
+import { newsSendToCheckService } from "../services/news.send.to.check.services.js";
 
 export const createNews = async (req, res) => {
 
@@ -58,6 +59,33 @@ export const getNewsJournalist = async (req, res) => {
 
         if (error.message === "NO_NEWS_FOUND") {
             return res.status(404).json({ message: "No se encontraron noticias con el estado indicado" });
+        }
+
+        return res.status(500).json({ message: "Error al obtener las noticias del periodista", error: error.message });
+    }
+};
+
+export const newsSendToCheck = async (req, res) => {
+
+    const journalistId = req.user.journalist_id;
+    const newsId = req.params.id;
+
+    try {
+        const news = await newsSendToCheckService(journalistId, newsId);
+
+        return res.status(200).json({
+            message: "OK",
+            news
+        });
+
+    } catch (error) {
+
+        if (error.message === "JOURNALIST_NOT_FOUND") {
+            return res.status(404).json({ message: "No se encontró el periodista con la ID indicada" });
+        }
+
+        if (error.message === "NEWS_NOT_FOUND") {
+            return res.status(404).json({ message: "No se encontró la noticia con la ID indicada" });
         }
 
         return res.status(500).json({ message: "Error al obtener las noticias del periodista", error: error.message });
