6cf392366c17b0cde113a1e6a922f513fb5ca058
evilrushgame <traviesogonzalo@gmail.com>
2026-01-08 13:03:15 -0300

    agregado indice de las rutas de noticias

---
+++ src/routes/news.routes.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+const router = Router();
+import upload from "../middlewares/multer.config.js";
+
+import {
+    createNews
+} from "../controllers/news.controllers.js";
+
+import { tokenMiddleware } from "../middlewares/token.middlewares.js";
+
+import { refreshToken } from "../middlewares/refresh.token.js";
+
+router.post("/create", tokenMiddleware, refreshToken, upload.single('photo'), createNews);
+
+export default router;
