1429491148bc3558048f08c58a35d8bee42522a7
evilrushgame <traviesogonzalo@gmail.com>
2026-01-09 10:09:23 -0300

    agregado funcion para subir videos

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -4,9 +4,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD,
-  api_key: process.env.CLOUDINARY_KEY,
-  api_secret: process.env.CLOUDINARY_SECRET
+    cloud_name: process.env.CLOUDINARY_CLOUD,
+    api_key: process.env.CLOUDINARY_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET
 });
 
 export const uploadImage = async (filePath, folder) => {
@@ -27,6 +27,28 @@ export const uploadImage = async (filePath, folder) => {
     } catch (error) {
         console.error("Cloudinary upload error:", error);
         throw new Error("IMAGE_UPLOAD_FAILED");
+    }
+};
+
+export const uploadVideo = async (filePath, folder) => {
+    try {
+        const result = await cloudinary.uploader.upload(filePath, {
+            folder,
+            resource_type: "video",
+            transformation: [
+                { quality: "auto" },
+                { fetch_format: "auto" }
+            ]
+        });
+
+        return {
+            url: result.secure_url,
+            public_id: result.public_id
+        };
+
+    } catch (error) {
+        console.error("Cloudinary upload error:", error);
+        throw new Error("VIDEO_UPLOAD_FAILED");
     }
 };
 
