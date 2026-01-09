18705ac2ba32f038fe5e5cf7f3a0d18ff1ba7f7f
evilrushgame <traviesogonzalo@gmail.com>
2026-01-09 11:02:32 -0300

    agregado public_id a news.video

--- src/models/news.video.js
+++ src/models/news.video.js
@@ -14,6 +14,10 @@ const NewsVideo = sequelize.define(
             type: DataTypes.STRING,
             allowNull: false
         },
+        public_id: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
         caption: {
             type: DataTypes.STRING,
             allowNull: true
