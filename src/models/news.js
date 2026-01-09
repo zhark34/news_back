3637340fb5116bccef030d4a8f6fad260dd427c8
evilrushgame <traviesogonzalo@gmail.com>
2026-01-09 13:22:38 -0300

    agregado cover_photo_public_id

--- src/models/news.js
+++ src/models/news.js
@@ -14,6 +14,10 @@ const News = sequelize.define(
             type: DataTypes.STRING(),
             allowNull: false,
         },
+        cover_photo_public_id: {
+            type: DataTypes.STRING(),
+            allowNull: true,
+        },
         category: {
             type: DataTypes.STRING(50),
             allowNull: false,
