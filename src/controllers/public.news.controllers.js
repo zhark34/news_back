19d7cd6fa154249d5dce73e48022ec15ea6b277e
evilrushgame <traviesogonzalo@gmail.com>
2026-01-13 15:24:14 -0300

    Se implementó public.news.controllers.js con funciones para obtener noticias por categoría, título y las más recientes.

---
+++ src/controllers/public.news.controllers.js
@@ -0,0 +1,68 @@
+import { getPublicNewsByCategoryService } from "../services/public.news.services.js";
+import { getPublicNewsByTitleService } from "../services/public.news.by.title.services.js";
+import { getLatestNewsService } from "../services/public.news.latest.services.js";
+
+export const getPublicNewsByCategory = async (req, res) => {
+
+    const { category } = req.params;
+
+    try {
+
+        const news = await getPublicNewsByCategoryService(category);
+
+        res.status(200).json(news);
+
+    } catch (error) {
+
+        if (error.message === "NOT_NEWS_FOUND") {
+            return res.status(404).json({ message: "No se encontraron noticias" });
+        }
+
+        return res.status(500).json({ message: "Error al obtener las noticias" });
+    }
+
+}
+
+export const getPublicNewsByTitle = async (req, res) => {
+
+    const { title } = req.params;
+
+    const decodedTitle = title.replace(/-/g, " ");
+
+    console.log(decodedTitle);
+
+    try {
+
+        const news = await getPublicNewsByTitleService(decodedTitle);
+
+        res.status(200).json(news);
+
+    } catch (error) {
+
+        if (error.message === "NOT_NEWS_FOUND") {
+            return res.status(404).json({ message: "No se encontraron noticias" });
+        }
+
+        return res.status(500).json({ message: "Error al obtener las noticias" });
+    }
+
+}
+
+export const getLatestNews = async (req, res) => {
+
+    try {
+
+        const news = await getLatestNewsService();
+
+        res.status(200).json(news);
+
+    } catch (error) {
+
+        if (error.message === "NOT_NEWS_FOUND") {
+            return res.status(404).json({ message: "No se encontraron noticias" });
+        }
+
+        return res.status(500).json({ message: "Error al obtener las noticias" });
+    }
+
+}
