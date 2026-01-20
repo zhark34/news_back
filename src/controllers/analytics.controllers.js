d2b84434ea89d4993adf4b2d35755f439607d751
evilrushgame <traviesogonzalo@gmail.com>
2026-01-19 22:34:11 -0300

    Implementar un sistema de análisis y puntuación de noticias con nuevos puntos finales públicos y de análisis.

--- src/controllers/analytics.controllers.js
+++ src/controllers/analytics.controllers.js
@@ -1,4 +1,8 @@
 import { createEventService } from "../services/analytics.events.services.js";
+import { getNewsMetricsService } from "../services/analytics.metrics.services.js";
+import { getNewsMetricsByNewsService } from "../services/analytics.metrics.by.news.services.js";
+import { getRankingJournalistService } from "../services/analytics.metrics.journalist.services.js";
+import { getReadingsService } from "../services/analytics.metrics.readings.services.js";
 
 
 export const createEvent = async (req, res) => {
@@ -13,6 +17,64 @@ export const createEvent = async (req, res) => {
 
     } catch (error) {
         return res.status(500).json({ message: "Error al crear el evento" });
+    }
+
+}
+
+export const getNewsMetrics = async (req, res) => {
+
+    try {
+
+        const eventCreated = await getNewsMetricsService();
+
+        return res.status(201).json(eventCreated);
+
+    } catch (error) {
+        return res.status(500).json({ message: "Error al crear el evento" });
+    }
+
+}
+
+export const getNewsMetricsByNews = async (req, res) => {
+
+    const { id } = req.params;
+
+    try {
+
+        const eventCreated = await getNewsMetricsByNewsService(id);
+
+        return res.status(201).json(eventCreated);
+
+    } catch (error) {
+        return res.status(500).json({ message: "Error al crear el evento" });
+    }
+
+}
+
+export const getRankingJournalist = async (req, res) => {
+
+    try {
+
+        const eventCreated = await getRankingJournalistService();
+
+        return res.status(201).json(eventCreated);
+
+    } catch (error) {
+        return res.status(500).json({ message: "Error al crear el evento" });
+    }
+
+}
+
+export const getReadings = async (req, res) => {
+
+    try {
+
+        const eventCreated = await getReadingsService();
+
+        return res.status(201).json(eventCreated);
+
+    } catch (error) {
+        return res.status(500).json({ message: "Error al obtener las lecturas" });
     }
 
 }
