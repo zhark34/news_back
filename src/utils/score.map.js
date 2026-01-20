80af0dc0ecaface65b1c96b131ab1c6dcd386be7
evilrushgame <traviesogonzalo@gmail.com>
2026-01-19 22:41:15 -0300

    Introduzca `SCORE_MAP` para definir pesos para las métricas de puntuación de artículos de noticias.

--- src/utils/score.map.js
+++ src/utils/score.map.js
@@ -1,19 +1,24 @@
 export const SCORE_MAP = {
-    total_time_seconds: 10,
-    active_views: 9,
-    scroll_100: 9,
-    scroll_75: 8,
-    scroll_50: 7,
-    scroll_25: 6,
-    sessions: 8,
-    total_bounce: 8,
-    total_exist: 7,
-    unique_views: 7,
-    page_views: 6,
-    trafic_organic: 7,
-    trafic_direct: 6,
-    trafic_referal: 5,
-    device_mobile: 4,
-    device_desktop: 3,
-    device_tablet: 2
+    total_time_seconds: 0.1,
+    active_views: 2,
+    scroll_100: 5,
+    scroll_75: 3,
+    scroll_50: 2,
+    scroll_25: 1,
+
+    sessions: 1,
+    page_views: 0.5,
+    unique_views: 1.5,
+
+    trafic_organic: 2,
+    trafic_direct: 1.5,
+    trafic_referral: 1,
+    trafic_social: 1,
+
+    total_bounces: -2,
+    total_exits: -1,
+
+    device_mobile: 0.5,
+    device_desktop: 0.5,
+    device_tablet: 0.3
 };
