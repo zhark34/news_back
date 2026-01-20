940fd9afb958b78b38f23758a3f9eabdc3fdd95d
evilrushgame <traviesogonzalo@gmail.com>
2026-01-19 22:35:08 -0300

    Implementar funciones de análisis de noticias, puntuación de noticias y clasificación de periodistas con nuevos servicios, rutas y funciones de utilidad.

--- src/routes/analytics.routes.js
+++ src/routes/analytics.routes.js
@@ -1,8 +1,26 @@
 import { Router } from "express";
 const router = Router();
 
-import { createEvent } from "../controllers/analytics.controllers.js";
+import {
+    createEvent,
+    getNewsMetrics,
+    getNewsMetricsByNews,
+    getRankingJournalist,
+    getReadings
+} from "../controllers/analytics.controllers.js";
+
+import { tokenMiddleware } from "../middlewares/token.middlewares.js";
+
+import { refreshToken } from "../middlewares/refresh.token.js";
 
 router.post('/event', createEvent)
+
+router.get('/news/metrics', tokenMiddleware, refreshToken, getNewsMetrics);
+
+router.get('/news/metrics-by-news/:id', tokenMiddleware, refreshToken, getNewsMetricsByNews);
+
+router.get('/journalist/ranking', tokenMiddleware, refreshToken, getRankingJournalist);
+
+router.get('/readings', tokenMiddleware, refreshToken, getReadings);
 
 export default router;
