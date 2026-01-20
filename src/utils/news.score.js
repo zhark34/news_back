44f6f0d895269c7f7559d12deb4747c46398db90
evilrushgame <traviesogonzalo@gmail.com>
2026-01-19 22:40:59 -0300

    Agregue una utilidad de puntuación de análisis de noticias y su mapa de ponderación de puntuación asociado.

--- src/utils/news.score.js
+++ src/utils/news.score.js
@@ -1,11 +1,17 @@
-import { analiticsScore } from "./analitics.score.js";
+import { analyticsScore } from "./analytics.score.js";
 
-export const newsScore = async (analitics) => {
+export const newsScore = async (analyticsArray) => {
+
+    if (!Array.isArray(analyticsArray) || analyticsArray.length === 0) return 0;
 
     var score = 0;
 
-    for (const [key, value] of Object.entries(analitics)) {
-        score += analiticsScore(key, value);
+    for (const dailyData of analyticsArray) {
+        const data = dailyData.toJSON ? dailyData.toJSON() : dailyData;
+
+        for (const [key, value] of Object.entries(data)) {
+            score += analyticsScore(key, value);
+        }
     }
 
     return score;
